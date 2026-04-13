fb6bf9b807c52c3063372013cf849d3b7fca3991
feeshkay <ktsrfishbeck@gmail.com>
2026-04-13 07:42:32 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'book-annotator-v1';
-const ASSETS = ['/', '/index.html', '/app.js', '/style.css', '/manifest.json'];
+const ASSETS = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
